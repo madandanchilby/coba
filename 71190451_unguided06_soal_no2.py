3d4a4585ef3d341369accb77dcc49b865bebc8e8
madandanchilby <79045191+madandanchilby@users.noreply.github.com>
2021-10-07 00:12:24 +0900

    fwgfuaf
    fafafaf

---
+++ 71190451_unguided06_soal_no2.py
@@ -0,0 +1,13 @@
+'''
+ukuran = int(input('ukuran:'))
+RangeDeret=ukuran**2
+for i in range (1,RangeDeret+1,1):
+    print(i,  '\t', end='')
+    if i % ukuran ==0
+        print()
+'''
+n1=10
+n2=4
+a=n1%n2
+print(a)
+    
